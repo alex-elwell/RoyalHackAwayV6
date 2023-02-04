55edef32f51abb1dd8574621cde0e11a38a88bc2
Lewis Chippendale <lewis.chippendale@hotmail.co.uk>
2023-02-04 21:52:57 +0000

    plant_display update 2

--- plantSmileyDisplay/smileyDisplayCode.py
+++ plantSmileyDisplay/smileyDisplayCode.py
@@ -1,18 +1,18 @@
 import sys
 from PyQt6.QtCore import Qt, QSize
-
+from PyQt6 import QtWidgets
 # Import necessary features of QtWidgets from the PyQt6 library
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QTabWidget, QGridLayout, QVBoxLayout
-from PyQt6.QtWidgets import QToolBar, QStatusBar
+from PyQt6.QtWidgets import QToolBar, QStatusBar, QDialog, QProgressBar
 from PyQt6.QtGui import QImage, QAction, QIcon, QPixmap
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-
-        self.setWindowTitle("My Awesome App")
-
+        self.x = 1 # Will be used to identify button input
+        self.setWindowTitle("Plant Display")
+        layout = QVBoxLayout()
 
         # Creates the toolbar
         toolbar = QToolBar("Toolbar Test")
@@ -22,31 +22,85 @@ class MainWindow(QMainWindow):
 
         # Button created in the toolbar
         leafButton = QAction(QIcon("leafButton.png"),"Plant Status Button", self)
-        leafButton.triggered.connect(self.plantStatusWindow)
+        self.setCentralWidget(self.defaultSmiley())
+        leafButton.triggered.connect(self.buttonSwitch)
         toolbar.addAction(leafButton)
 
         # Shows the purpose of the button
         leafButton.setStatusTip("Show Plant Moisture Status")
         self.setStatusBar(QStatusBar(self))
 
+
+
+
+    def buttonSwitch(self, labelval):
+        # If statements check what the label is
+
+        if self.x == 2:
+            print("2")
+            self.defaultSmiley()
+        elif self.x == 1:
+            print("1")
+            self.plantStatusWindow()
+        else:
+            print("Error")
+
+    def defaultSmiley(self):
+        layout = QVBoxLayout()
         # Creates a label with the smiley faces on it
+        self.x = 1
         label = QLabel(self)
+        widget = QWidget()
         smiley = QPixmap("calmBaseFace.png") # Calm smiley is default smiley
         label.setPixmap(smiley)
-        self.setCentralWidget(label)
+
+        layout.addWidget(label)
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+        self.show()
+
 
 
     # Status section will be visible to the user to see the stats of the plant
-    def plantStatusWindow(self, s):
-        print("click", s)
+    def plantStatusWindow(self):
+        layout = QVBoxLayout()
+        self.x = 2
+        print("click")
+        widget = QWidget(self)
+
+        #Creating progress bars for each plant
+        pbar = QProgressBar(self)
+        pbar.setValue(50) # Value can be set based on the moisture input
+        layout.addWidget(pbar)
 
 
-app = QApplication(sys.argv)
+        pbar2 = QProgressBar(self)
+        pbar2.setValue(20)  # Value can be set based on the moisture input
+        layout.addWidget(pbar2)
 
-window = MainWindow()
-window.show()
-window.showMaximized()
-sys.exit(app.exec())
+        pbar3 = QProgressBar(self)
+        pbar3.setValue(75)  # Value can be set based on the moisture input
+        layout.addWidget(pbar3)
+
+        #widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+        self.show()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    #widget = QtWidgets.QStackedWidget()
+    window = MainWindow()
+
+    window.show()
+    window.showMaximized()
+    sys.exit(app.exec())
 
 
 
